7ad1d6f35aa5f80202dfb071359ccc547c9827f1
odalmer <48610363+odalmer@users.noreply.github.com>
2021-01-23 22:09:35 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -9,32 +9,26 @@ let level = 0, thisG = 0, thatG = 1, nameG = 0, img = 1;
 
 let games = [
     // level1
-    [['fortnite', "https://image.api.playstation.com/vulcan/img/rnd/202012/0206/oZzgDgvDYaNFGWZnrl9hRdG3.png"], ['warzone', "https://www.teknofilo.com/wp-content/uploads/2020/03/CoD-Warzone1.jpg"]],
+    [['fortnite', "imgs/fortnite.jpg"], ['warzone', "imgs/Warzone.jpg"]],
 
     // level 2
-    [['fallGuys', 'https://blog.latam.playstation.com/tachyon/sites/3/2020/08/50167897647_4dbfde6c5a_oa.jpg?resize=1088,612&crop_strategy=smart'],
-
-    ['amongUs', 'https://www.emprendedores.es/wp-content/uploads/2020/10/Among-Us-1200x675-1.jpg']],
+    [['fallGuys', 'imgs/maxresdefault.jpg'], ['amongUs', 'imgs/Among-Us-1200x675-1.jpg']],
 
     // level 3
-    [['lol', 'https://esports.eldesmarque.com/wp-content/uploads/2019/09/LoL2.jpg'],
-
-    ['dota', 'https://hipertextual.com/files/2013/07/dota-2-para-mac-y-linux.jpg']],
+    [['lol', 'imgs/LoL2.jpg'], ['dota', 'imgs/dota-2-para-mac-y-linux.jpg']],
 
     // level 4
-    [['brawlhalla', 'https://i.blogs.es/e1da3c/brawlhalla/1366_2000.jpeg'],
-
-    ['smash', 'https://www.smashbros.com/assets_v2/img/movie/20180613_1.jpg']],
+    [['brawlhalla', 'imgs/1366_2000.jpeg'], ['smash', 'imgs/20180613_1.webp']],
 
     // level 5
-    [['batman', 'https://cdn.cloudflare.steamstatic.com/steam/apps/208650/capsule_616x353.jpg?t=1591738126'], ['spiderman', 'https://image.api.playstation.com/vulcan/ap/rnd/202011/0402/pZ2pIEEnH7YhEtpxh1CY6KDz.png']],
+    [['batman', 'imgs/capsule_616x353.jpg'], ['spiderman', 'imgs/pZ2pIEEnH7YhEtpxh1CY6KDz.webp']],
 
     // level 6
-    [['paladins', 'https://i.ytimg.com/vi/0rV3-HXhky8/maxresdefault.jpg'],
-    ['overwatch', 'https://i.blogs.es/9e7c92/captura-de-pantalla-2016-05-22-a-las-19.16.39/840_560.png']],
+    [['paladins', 'imgs/paladins.jpg'],
+        ['overwatch', 'imgs/840_560.png']],
 
     // level 7
-    [['cod', 'https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/zeus/common/social-share/zeus-social-share.jpg'], ['battlefield', 'https://image.api.playstation.com/cdn/EP0006/CUSA08670_00/l2B6pYvxHDYUjuhT0vTHcidJA2MpjWR4.png']]
+    [['cod', 'imgs/download.jfif'], ['battlefield', 'imgs/battlefield.jfif']]
 
     // level 8
 
